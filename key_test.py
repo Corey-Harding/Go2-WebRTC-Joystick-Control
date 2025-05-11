3e946fee6b88e63ad19598acba71747cd0b2e78d
Corey-Harding <91717711+Corey-Harding@users.noreply.github.com>
2025-05-11 07:51:06 +0000

    Add files via upload

--- key_test.py
+++ key_test.py
@@ -1,24 +1,9 @@
-# Unitree Go2 Webrtc Controller by Corey Harding
-# Requires https://github.com/legion1581/go2_webrtc_connect
-# Includes code from Legion1581 https://github.com/legion1581/go2_webrtc_connect
-# Unfinished crude implementation of the wireless_controller
-
-import asyncio
-import threading
-import pygame
-from time import time
-import ctypes
-import json
-from go2_webrtc_driver.webrtc_driver import Go2WebRTCConnection, WebRTCConnectionMethod
-from go2_webrtc_driver.constants import RTC_TOPIC, SPORT_CMD
-from config_file import *
-import os
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide" # Disable pygame welcome message
+# Joystick test by Corey Harding
+# Test your joystick in pygame to make configuraiton via config_file.py easier
 
 import asyncio
 import pygame
 from time import time
-import numpy as np
 from config_file import *
 
 async def main():
@@ -43,14 +28,7 @@ async def main():
     BITMASK_D_PAD_DOWN =   0b0100000000000000
     BITMASK_D_PAD_LEFT =   0b1000000000000000
 
-    Lx = 0
-    Ly = 0
-    Rx = 0
-    Ry = 0
-
-
     try:
-        #Initialize pygame joystick support
         pygame.init()
         clock = pygame.time.Clock()
         if pygame.joystick.get_count() == 0:
@@ -60,21 +38,17 @@ async def main():
         joystick.init()
         print("Joystick Initialized...")
 
-        #Initialize go2_webrtc_driver
-        conn = WEBRTC_CONNECTION_TYPE
-        await conn.connect() # Connect to the WebRTC service.
-
-        #print(f"Keys variable initialized as: {bin(keys)[2:].zfill(16)}")
-
+        print(f"Keys variable initialized as: {bin(keys)[2:].zfill(16)}")
+        
     except ValueError as e:
         # Log any value errors that occur during the process.
         logging.error(f"An error occurred: {e}")
         print(f"An error occurred: {e}")
 
     run = True
-
     while run:
         for event in pygame.event.get():
+
             #Quit pygame while loop
             if event.type == pygame.QUIT:
                 print("Closing application...")
@@ -227,56 +201,20 @@ async def main():
                         print(f"RIGHT_TRIGGER_AXIS Postion: ({str(event.value)})")
                         keys &= ~ BITMASK_R2 # Set bit to 0
                         print(f"Keys variable: {bin(keys)[2:].zfill(16)}")
+                elif event.value > DEADZONE or event.value < -DEADZONE:
+                    print(f"AXIS: ({str(event.axis)}) POSITION: ({str(event.value)})")
+                    if event.axis == LEFT_X_AXIS:
+                        print(f"LEFT_X_AXIS Postion: ({str(event.value)})")
+                    if event.axis == LEFT_Y_AXIS:
+                        print(f"LEFT_Y_AXIS Postion: ({str(event.value)})")
+                    if event.axis == RIGHT_X_AXIS:
+                        print(f"RIGHT_X_AXIS Postion: ({str(event.value)})")
+                    if event.axis == RIGHT_Y_AXIS:
+                        print(f"RIGHT_Y_AXIS Postion: ({str(event.value)})")
 
-        Lx = joystick.get_axis(LEFT_X_AXIS)*Lxsensitivity # Left analog stick X-axis
-        Ly = -joystick.get_axis(LEFT_Y_AXIS)*Lysensitivity # Left analog stick Y-axis
-        Rx = joystick.get_axis(RIGHT_X_AXIS)*Rxsensitivity # Right analog stick X-axis
-        Ry = -joystick.get_axis(RIGHT_Y_AXIS)*Rysensitivity # Right analog stick Y-axis
-        if (abs(Lx) > DEADZONE or abs(Ly) > DEADZONE or abs(Rx) > DEADZONE):
-            try:
-                print(f"Sending command( lx: {Lx}, ly: {Ly}, rx: {Rx}, ry: {Ry}, keys: 0 )")
-                response = await asyncio.wait_for(conn.datachannel.pub_sub.publish(
-                RTC_TOPIC["WIRELESS_CONTROLLER"],
-                {
-                    "lx": Lx,
-                    "ly": Ly,
-                    "rx": Rx,
-                    "ry": Ry,
-                    "keys": 0
-                }
-                ),
-                timeout=.1
-                )
-                print(f"Response: {response}")
-                await asyncio.sleep(.1)
-            except asyncio.TimeoutError:
-                print("Timeout Error")
-
-        if keys > 0:
-            try:
-                print(f"Sending command( lx: 0, ly: 0, rx: 0, ry: 0, keys: {keys} )")
-                response = await asyncio.wait_for(conn.datachannel.pub_sub.publish(
-                RTC_TOPIC["WIRELESS_CONTROLLER"],
-                {
-                    "lx": 0,
-                    "ly": 0,
-                    "rx": 0,
-                    "ry": 0,
-                    "keys": keys
-                }
-                ),
-                timeout=.1
-                )
-                print(f"Response: {response}")
-                await asyncio.sleep(.5)
-            except asyncio.TimeoutError:
-                print("Timeout Error")
-
-        #Workaround to keep webrtc connection from randomly disconnecting
-        def lowstate_callback(message):
-            current_message = message['data']
-        conn.datachannel.pub_sub.subscribe(RTC_TOPIC['LOW_STATE'], lowstate_callback)
-        await asyncio.sleep(.1)
+#            Joyball Not Tested
+#            elif event.type == pygame.JOYBALLMOTION:
+#                print(f"JOYBALL POSITION: ({str(event.value)})")
 
         clock.tick(CONTROLLER_FPS_LIMIT) # Limit how many times a second the script is able to run
         
